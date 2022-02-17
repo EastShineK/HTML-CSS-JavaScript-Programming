8f00ed39f380e309d6bbde2867f611f8d7d826a7
EastShineK <ehdqls0212@naver.com>
2022-02-17 21:04:28 +0900

    Life Coding React Day 2 - make component 1

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,11 +2,22 @@ import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
 
+class Subject extends Component {
+  render() {
+    return(
+      <header>
+          <h1>WEB</h1>
+          world wide web!
+      </header>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, React!!!
+        <Subject></Subject>
       </div>
     );
   }
