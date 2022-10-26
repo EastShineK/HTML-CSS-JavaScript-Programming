4165fc44e4caa2b654ac6ab35a27881615c06dd1
EastShineK <ehdqls0212@naver.com>
2022-10-26 21:30:48 +0900

    Make function

--- Vanilla JS_Make Chrome App/app.js
+++ Vanilla JS_Make Chrome App/app.js
@@ -1,8 +1,17 @@
 const player = {
-    name: "nico",
-    points: 10,
-    fat: true,
+  name: "nico",
+  points: 10,
+  fat: true,
+  sayHi: function (otherPersonName) {
+    console.log("hello " + otherPersonName + " nice to meet you!");
+  },
 };
+function sayHello(nameOfPerson, age) {
+  console.log("Hello my name is " + nameOfPerson + " and I'm " + age + " old");
+}
+function plus(a, b) {
+  console.log(a + b);
+}
 console.log(player);
 console.log(player.name);
 console.log(player["name"]);
@@ -12,3 +21,6 @@ player.lastName = "potato";
 console.log(player);
 player.points = player.points + 10;
 console.log(player.points);
+sayHello("nico", 22);
+plus(4, 12);
+player.sayHi("lynn");
