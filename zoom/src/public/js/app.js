aa920aa44bb3207a66b249cbf4a08e9c1b8c1137
EastShineK <ehdqls0212@naver.com>
2023-03-20 17:01:16 +0900

    Implement basic WebSocket Messages

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,1 +1,16 @@
 const socket = new WebSocket(`ws://${window.location.host}`); //socket -> connection to server
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from the Server ❌");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 10000);
